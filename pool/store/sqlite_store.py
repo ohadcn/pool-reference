059999b8fdf83ece1e6d6f933c4ade2d3f0bb442
Ohad Cohen <ohadcn@gmail.com>
2021-12-05 22:16:50 +0200

    split times by minute

--- pool/store/sqlite_store.py
+++ pool/store/sqlite_store.py
@@ -276,7 +276,12 @@ class SqlitePoolStore(AbstractPoolStore):
 
     async def get_points_by_date(self, launcher_id: bytes32, count: int) -> List[Tuple[uint64, uint64]]:
         cursor = await self.connection.execute(
-            "SELECT strftime('%H:00',datetime(timestamp, 'unixepoch'), 'localtime') as date, SUM(difficulty) as points from partial WHERE launcher_id=? AND strftime('%s', 'now','-1 day')<timestamp GROUP BY strftime('%Y.%m.%d.%H',datetime(timestamp, 'unixepoch')) LIMIT ?",
+            """
+            SELECT strftime('%H:%M',datetime(timestamp, 'unixepoch'), 'localtime') as date,
+            SUM(difficulty) as points
+            FROM partial WHERE launcher_id=? AND strftime('%s', 'now','-1 day')<timestamp
+            GROUP BY strftime('%Y.%m.%d.%H.%M',datetime(timestamp, 'unixepoch')) LIMIT ?
+            """,
             (launcher_id.hex(), count),
         )
         rows = await cursor.fetchall()
