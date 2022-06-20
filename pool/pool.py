2aa98af7a4778033e48738b7b6ac3fb16eee5685
Ohad Cohen <ohadcn@gmail.com>
2022-06-20 11:40:39 +0300

    minor fixes

--- pool/pool.py
+++ pool/pool.py
@@ -468,7 +468,7 @@ class Pool:
                 # TODO(pool): make sure you have enough to pay the blockchain fee, this will be taken out of the pool
                 # fee itself. Alternatively you can set it to 0 and wait longer
                 # blockchain_fee = 0.00001 * (10 ** 12) * len(payment_targets)
-                blockchain_fee: uint64 = uint64(1)
+                blockchain_fee: uint64 = uint64(1) * len(payment_targets)
                 try:
                     transaction: TransactionRecord = await self.wallet_rpc_client.send_transaction_multi(
                         self.wallet_id, payment_targets, fee=blockchain_fee
---
+++ run_forever.sh
@@ -0,0 +1,5 @@
+trap 'select wtd in bash restart exit; do [ $wtd = restart ] && break || $wtd ; done' \
+	2;
+while true; do
+	    timeout 1d python3 -m pool
+     done
